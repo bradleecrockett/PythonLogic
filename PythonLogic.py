57f509135b97ea47739f9f688373929fa8574366
bradleecrockett <19703290+bradleecrockett@users.noreply.github.com>
2017-10-25 15:11:59 -0700

    Add files via upload

---
+++ PythonLogic.py
@@ -0,0 +1,60 @@
+# Name: First Last
+# Date:
+# Period:
+# Lab: PythonLogic.py
+# Description: Write the 4 functions described below in comments
+#
+#     Style - Code format, whitespace and PEP-8 style is followed making code easy to read.
+#     Comments - Blocks of code are well commented, every function has a descriptive comment.
+#     Tests -   The program runs as described in the specifications without errors(passes all tests).
+#
+
+# The number 6 is great number.
+# Given two int values, a and b, return True if either one is 6.
+# Or if their sum or difference is 6.
+# Note: the function abs(num) computes the absolute value of a number.
+def love_six(a: int, b: int):
+    '''Your code below here'''
+
+    return 0
+
+
+# Given a day of the week encoded as 0=Sun, 1=Mon, 2=Tue, ...6=Sat, and a
+# boolean indicating if we are on vacation, return a string of the form "7:00"
+# indicating when the alarm clock should ring. Weekdays, the alarm should
+# be "7:00" and on the weekend it should be "10:00".
+# Unless we are on vacation -- then on weekdays it should be "10:00" and
+# weekends it should be "off"
+def alarm_clock(day: int, vacation: bool):
+    '''Your code below here'''
+
+    return 0
+
+
+# For this problem, we'll round an int value up to the next multiple of 10
+# if its rightmost digit is 5 or more, so 15 rounds up to 20. Alternately,
+# round down to the previous multiple of 10 if its rightmost digit is less
+# than 5, so 12 rounds down to 10. Given 3 ints, a b c, return the sum of
+# their rounded values.
+#
+# To avoid code repetition, write a separate helper "def round10(num):"
+# and call it 3 times. Write the helper entirely below and at the same
+# indent level as round_sum().
+def round_sum(a: int, b: int, c: int):
+    '''Your code below here'''
+
+    return 0
+
+
+# Given 3 int values, a b c, return their sum. However, if one of the values
+# is the same as another of the values, it does not count towards the sum.
+def lone_sum(a: int, b:int, c: int):
+    '''Your code below here'''
+
+    return 0
+
+
+# def main():
+#     '''Write some testing / debugging code here comment it out before submitting'''
+#
+# main()
